1cbb0aae813d72488adc4826eb6fd470c6d0a07d
Arun <iyur.smilx@gmail.com>
2025-11-18 16:34:44 +0530

    Updated

--- tabview/app/_layout.jsx
+++ tabview/app/_layout.jsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+export default function RootLayout() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>RootLayout</Text>
     </View>
   )
 }
